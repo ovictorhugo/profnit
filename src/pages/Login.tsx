bdc7b1692893bf684e24be0537a19cd2395196af
Victor Hugo <64437884+ovictorhugo@users.noreply.github.com>
2023-10-02 08:33:27 -0300

    203

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,6 +7,7 @@ export function Login() {
         <div>
           <Header/>
           <ContentLogin/>
+          
         </div>
     )
 }
--- src/pages/Magazine.tsx
+++ src/pages/Magazine.tsx
@@ -1,10 +1,12 @@
 import { ContentMagazine } from "../components/ContentMagazine";
+import { Footer } from "../components/Footer";
 
 export function Magazine() {
 
     return  (
         <div>
           <ContentMagazine/>
+          <Footer/>
         </div>
     )
 }
